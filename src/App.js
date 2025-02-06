6d26487e89d3b499aceb7eb690713f2bde59f278
itsskristyy <kristydelacruzx@gmail.com>
2025-02-05 19:27:12 -0500

    project finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home/Home";
+import Connect from "../src/components/Connect/Connect";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Projects from "../src/components/Projects/Projects";
+import Navigation from "../src/components/Navigation/Navigation";
+import { Route, Routes, useLocation } from "react-router-dom";
+import React from "react";
+import "./App.css";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/** new commit */}
+      {location.pathname !== "/" && <Navigation />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
+        {/* <Route path="/resume" element={<Resume />} /> */}
+        <Route path="/connect" element={<Connect />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav-elements">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/aboutme">About Me</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects"> Projects </NavLink>
+            </li>
+            {/* <li>
+              <NavLink to="/resume"> Resume </NavLink>
+            </li> */}
+            <li>
+              <NavLink to="/connect">Connect With Me</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
